5e0891a91af667077aad03c414dd9c65be677a5c
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-08-31 20:25:44 +0200

    get description id from description component if possible

--- Common/src/main/java/fuzs/enchantmentinsights/common/client/handler/EnchantedItemTooltipHandler.java
+++ Common/src/main/java/fuzs/enchantmentinsights/common/client/handler/EnchantedItemTooltipHandler.java
@@ -42,56 +42,56 @@ public final class EnchantedItemTooltipHandler extends TooltipDescriptionsHandle
 
     public static Map<String, EnchantmentWithLevel> getByDescriptionId(Stream<EnchantmentWithLevel> stream) {
         // an item can contain the same effect multiple times, so make sure to include a merge function in our collect call
-        return stream.mapMulti((EnchantmentWithLevel enchantmentWithLevel, Consumer<Map.Entry<String, EnchantmentWithLevel>> consumer) -> {
-                    Component component = enchantmentWithLevel.enchantment().value().description();
-                    if (component.getContents() instanceof TranslatableContents translatableContents) {
-                        consumer.accept(Map.entry(translatableContents.getKey(), enchantmentWithLevel));
+        return stream.mapMulti((EnchantmentWithLevel enchantment, Consumer<Map.Entry<String, EnchantmentWithLevel>> consumer) -> {
+                    // TODO map this to the description component directly without extracting a key in future versions
+                    Component component = enchantment.enchantment().value().description();
+                    if (component.getContents() instanceof TranslatableContents contents) {
+                        consumer.accept(Map.entry(contents.getKey(), enchantment));
                     }
                 })
                 .collect(Collectors.toMap(Map.Entry::getKey,
                         Map.Entry::getValue,
-                        (EnchantmentWithLevel o1, EnchantmentWithLevel o2) -> o1));
+                        (EnchantmentWithLevel o1, EnchantmentWithLevel o2) -> o2));
     }
 
     @Override
-    protected Component getValueComponent(EnchantmentWithLevel enchantmentWithLevel) {
+    protected Component getValueComponent(EnchantmentWithLevel enchantment) {
         // Replace the enchantment name with our colored variant.
-        return getFullName(enchantmentWithLevel.enchantment(), enchantmentWithLevel.level());
+        return getFullName(enchantment.enchantment(), enchantment.level());
     }
 
     /**
      * @see Enchantment#getFullname(Holder, int)
      */
     public static Component getFullName(Holder<Enchantment> enchantment, int level) {
-        MutableComponent mutableComponent = enchantment.value().description().copy();
-        mergeEnchantmentStyle(enchantment, mutableComponent);
-        addLevelComponent(enchantment, level, mutableComponent);
-        return mutableComponent;
+        MutableComponent component = enchantment.value().description().copy();
+        mergeEnchantmentStyle(enchantment, component);
+        addLevelComponent(enchantment, level, component);
+        return component;
     }
 
-    private static void mergeEnchantmentStyle(Holder<Enchantment> enchantment, MutableComponent mutableComponent) {
+    private static void mergeEnchantmentStyle(Holder<Enchantment> enchantment, MutableComponent component) {
         if (enchantment.is(EnchantmentTags.CURSE)) {
-            ComponentUtils.mergeStyles(mutableComponent,
+            ComponentUtils.mergeStyles(component,
                     EnchantmentInsights.CONFIG.get(ClientConfig.class).enchantedItemTooltips.enchantmentNameStyling.curseStyle);
         } else if (enchantment.is(EnchantmentTags.TREASURE)) {
-            ComponentUtils.mergeStyles(mutableComponent,
+            ComponentUtils.mergeStyles(component,
                     EnchantmentInsights.CONFIG.get(ClientConfig.class).enchantedItemTooltips.enchantmentNameStyling.treasureStyle);
         } else {
-            ComponentUtils.mergeStyles(mutableComponent,
+            ComponentUtils.mergeStyles(component,
                     EnchantmentInsights.CONFIG.get(ClientConfig.class).enchantedItemTooltips.enchantmentNameStyling.defaultStyle);
         }
     }
 
-    private static void addLevelComponent(Holder<Enchantment> enchantment, int level, MutableComponent mutableComponent) {
+    private static void addLevelComponent(Holder<Enchantment> enchantment, int level, MutableComponent component) {
         boolean maximumLevel = EnchantmentInsights.CONFIG.get(ClientConfig.class).enchantedItemTooltips.itemTooltipLines.maximumLevel();
 
         if (maximumLevel || level != 1 || enchantment.value().getMaxLevel() != 1) {
-            mutableComponent.append(CommonComponents.SPACE)
-                    .append(Component.translatable("enchantment.level." + level));
+            component.append(CommonComponents.SPACE).append(Component.translatable("enchantment.level." + level));
 
             if (maximumLevel) {
                 int maxLevel = enchantment.value().getMaxLevel();
-                mutableComponent.append(CommonComponents.SPACE)
+                component.append(CommonComponents.SPACE)
                         .append("(")
                         .append(Component.translatable("enchantment.level." + maxLevel))
                         .append(")");
